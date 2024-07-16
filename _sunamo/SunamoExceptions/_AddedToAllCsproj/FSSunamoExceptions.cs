c5f6c479794e071314c6c97eebb7e8ca16ee5666
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 15:35:23 +0200

    buildable, before standardize

--- _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
@@ -1,5 +1,7 @@
-namespace SunamoMime._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FSSunamoExceptions
+//namespace SunamoMime._sunamo.SunamoExceptions._AddedToAllCsproj;
+
+//namespace SunamoMime._sunamo.SunamoExceptions._AddedToAllCsproj;
+internal class FS
 {
     internal static void CreateUpfoldersPsysicallyUnlessThere(string nad)
     {
--- _sunamo/SunamoExceptions/_AddedToAllCsproj/SHSunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/SHSunamoExceptions.cs
@@ -1,6 +1,6 @@
-namespace SunamoMime._sunamo.SunamoExceptions._AddedToAllCsproj;
+//namespace SunamoMime._sunamo.SunamoExceptions._AddedToAllCsproj;
 
-internal class SHSunamoExceptions
+internal class SH
 {
     internal static string JoinNL(List<string> l)
     {

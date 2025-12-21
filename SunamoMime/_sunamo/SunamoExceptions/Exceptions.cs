4f97cd5fa7545cf48192d6178d97f8e60d96ce26
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:22:06 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoMime/_sunamo/SunamoExceptions/Exceptions.cs
+++ SunamoMime/_sunamo/SunamoExceptions/Exceptions.cs
@@ -1,6 +1,7 @@
+namespace SunamoMime._sunamo.SunamoExceptions;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoMime._sunamo.SunamoExceptions;
 // © www.sunamo.cz. All Rights Reserved.
 internal sealed partial class Exceptions
 {
